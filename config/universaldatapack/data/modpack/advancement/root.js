ac51574693641be8b13606394d91033e4054515f
Necrifer <hiizhexi@gmail.com>
2026-06-26 21:37:31 +0800

    Add main story advancement(root)

---
+++ config/universaldatapack/data/modpack/advancement/root.js
@@ -0,0 +1,17 @@
+{
+  "display": {
+    "icon": {
+      "id": "kubejs:eyes1"
+    },
+    "title": "Main Story",
+    "description": "The Eyes, what do they mean?",
+    "frame": "task",
+    "show_toast": true,
+    "announce_to_chat": true
+  },
+  "criteria": {
+    "root": {
+      "trigger": "minecraft:tick"
+    }
+  }
+}
