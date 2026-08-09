2dec01b5c51c7c71e51678458bfe5dc84ac6efc7
Necrifer <hiizhexi@gmail.com>
2026-08-09 22:52:20 +0800

    Update modpack backup 2026-08-09 22:52

--- KubeJS/server_scripts/empowerer.js
+++ KubeJS/server_scripts/empowerer.js
@@ -1,12 +1,24 @@
 ServerEvents.recipes(event => {
-    event.recipes.actuallyadditions
-        .empowering("mysticalagriculture:imperium_farmland", "minecraft:dirt", [
+    const empowerer = [
+        {
+          result: "mysticalagriculture:imperium_farmland",
+          base: "minecraft:dirt",
+          modifiers: [
             "enderio:cloud_seed_bucket",
             "mysticalagriculture:imperium_essence",
             "ae2:fluix_pearl",
             "divinerpg:shadow_bar",
-        ])
-        .energy(2000)
-        .color(Color.RED.argb)
-        .time(200)
+          ],
+          energy: 2000,
+          color: Color.RED.argb,
+          time: 200
+        }
+    ];
+    empowerer.forEach(empowerer =>
+        event.recipes.actuallyadditions.empowering(
+            empowerer.result, empowerer.base, empowerer.modifiers)
+            .energy(empowerer.energy)
+            .color(empowerer.color)
+            .time(empowerer.time)
+        )
 })
