36bacc57eccb93c9a9169ec07424aa27f86853e5
Necrifer <hiizhexi@gmail.com>
2026-08-27 17:31:54 +0800

    Add item renaming for localization in lang.js

---
+++ v1.20.1/kubejs/client_scripts/lang.js
@@ -0,0 +1,7 @@
+ClientEvents.lang('en_us', (event) => {
+    event.renameItem('aoa3:ghastly_ingot', 'Twilight dipped Ingot');
+    event.renameItem('castle_in_the_sky:laputa_core_orb', 'Laputa Core Orb')
+    event.renameItem('minecraft:foundational_breaker', 'Foundational Breaker')
+    event.renameItem('minecraft:anomalous_condenser', 'Anomalous Condenser')
+    event.renameItem('minecraft:soul_capturer', 'Soul Capturer')
+})
