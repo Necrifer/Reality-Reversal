97a731c44311ce087b8bdc07155c1b33e99726ff
Necrifer <hiizhexi@gmail.com>
2026-08-30 15:28:27 +0800

    Changed recipe regarding custom aura

--- v1.20.1/kubejs/startup_scripts/nature_aura.js
+++
@@ -1,7 +0,0 @@
-NaturesAuraEvents.init((event) => {
-    // custom aura type (aura type, dimension, aura colour, priority-optional)
-    event.custom('kubejs:nihil', 'javd:void', 0xC22400)
-
-    // modify the aura type of the dimension (dimension, aura type)
-    // event.modify('minecraft:overworld', 'kubejs:aether')
-})
